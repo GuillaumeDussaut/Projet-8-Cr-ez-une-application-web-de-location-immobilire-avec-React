cac34c365b5318da1809c94a3ce680615bab6b47
Guillaume <74982048+GuillaumeDussaut@users.noreply.github.com>
2023-06-30 16:13:01 +0200

    MAJ
    Mise en place de sass.
    Ajout des propriétées css pour :
        - le container des vignettes
        - les vignettes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Bandeau from './components/Header/bandeau';
+import Locations from './components/location-container/container';
 import './App.scss';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header/>
       <Bandeau/>
+      <Locations/>
       <Footer/>
     </div>
   );
---
+++ src/components/location-container/container.js
@@ -0,0 +1,19 @@
+
+
+export default function locationContainer(){
+    return (
+        <div className="location">
+            <div className="location-Container">
+                <div className="vignette">
+                    <p>Titre de la location</p>
+                </div>
+                <div className="vignette"></div>
+                <div className="vignette"></div>
+                <div className="vignette"></div>
+                <div className="vignette"></div>
+                <div className="vignette"></div>
+            </div>
+        </div>
+        
+    );
+}
