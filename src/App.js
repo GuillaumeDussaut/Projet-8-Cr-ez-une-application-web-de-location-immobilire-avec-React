ba4d56dd707c5643f89fd69808fcae5d03c9e915
Guillaume <74982048+GuillaumeDussaut@users.noreply.github.com>
2023-07-22 16:33:36 +0200

    maj
    ajustement de quelques propriétés css

--- src/App.js
+++ src/App.js
@@ -5,29 +5,22 @@ import HeaderAccueil from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import AproposContainer from "./pages/Apropos";
 import Error404 from "./pages/404";
-import Accueil from "./pages/accueil";
+import Accueil from "./pages/home";
 import Location from "./pages/location";
 
 import "./App.scss";
-
 function App() {
   return (
     <BrowserRouter className="App">
-      <HeaderAccueil />
+      <HeaderAccueil/>
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/apropos" element={<AproposContainer />} />
         <Route path="/location/:id" element={<Location />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
-      <Footer />
+      <Footer/>
     </BrowserRouter>
   );
 }
-
 export default App;
-
-
-
-
-
